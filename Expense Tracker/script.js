b86a9526eb039e93d6f11faf196e126a2783c4b5
Herbert Saladar <hrbrtsldr@gmail.com>
2021-05-10 23:28:29 +0800

    Add Expense Tracker

---
+++ Expense Tracker/script.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById('balance')
+const money_plus = document.getElementById('money-plus')
+const money_minus = document.getElementById('money-minus')
+const list = document.getElementById('list')
+const from = document.getElementById('from')
+const text = document.getElementById('text')
+const amount = document.getElementById('amount')
