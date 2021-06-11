5c8c471c6d7f893b8b22e710444fb38eca2f66fb
Herbert Saladar <hrbrtsldr@gmail.com>
2021-06-11 16:45:55 +0800

    Add time change in every dificulty

--- Typing Game/script.js
+++ Typing Game/script.js
@@ -3,12 +3,10 @@ const text = document.getElementById('text')
 const scoreEL = document.getElementById('score')
 const timeEl  =  document.getElementById('time')
 const endgameEl  =  document.getElementById('end-game-container')
-
 const settingsBtn = document.getElementById('settings-btn')
 const settings = document.getElementById('settings')
 const settingsForm = document.getElementById('settings-form')
 const difficultSelect = document.getElementById('difficulty')
-
 
 const words = [
     'sigh',
@@ -19,27 +17,34 @@ const words = [
 let randomWord;
 let score = 0
 let time = 10
+//Set difficultu to value in ls or medium
+let diffuculty = localStorage.getItem('difficulty') != null ? localStorage.getItem('difficulty') : 'medium' 
 
+difficultSelect.value = diffuculty
 //Countdown
 const timeInterval = setInterval(() => {
     updateTime()
 }, 1000);
 
+//Get Random World
 function getRandomWord(){
     return words[Math.floor(Math.random() * words.length )]
 }
 
+//Add Word to DOM object 
 function addWordtoDOM(){
     randomWord =  getRandomWord()
     text.focus()
     word.innerHTML = randomWord
 }
 
+//Update Score
 function updateScore(){
     score++;
     scoreEL.innerHTML = score
 }
 
+//Update time and clear time if time has reached to 0
 function updateTime(){
     time--;
     timeEl.innerHTML = time+'s'
@@ -51,7 +56,7 @@ function updateTime(){
     }
 }
 
-//Show Endscreen
+//Show End Screen
 function gameOver(){
     endgameEl.innerHTML = `<h1>Time Ran out</h1>
     <p> Your final score is ${score}</p>
@@ -61,13 +66,41 @@ function gameOver(){
     console.log( 'game over')
 }
 
-addWordtoDOM()
+
+// On input text
 text.addEventListener('input', e=>{
     const insertedText = e.target.value
     if( insertedText == randomWord ){
         addWordtoDOM()
         updateScore()
         e.target.value = ""
-        time += 5
+
+        switch(diffuculty){
+            case 'easy':
+                time += 5
+            break;
+            case 'medium':
+                time += 3
+
+            break;
+            case 'hard':
+                time += 2
+            break;
+
+        }
     }
 })
+
+settingsBtn.addEventListener('click', () => {
+    settings.classList.toggle('hide')
+
+})
+
+//settings select
+settingsForm.addEventListener('change', e => {
+    diffuculty = e.target.value
+    localStorage.setItem('difficulty', diffuculty)
+})
+
+
+addWordtoDOM()
